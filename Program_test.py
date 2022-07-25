1a19c7497b553f4495d920a427c33cd32cb4afef
pythonxl <bircan.math@gmail.com>
2022-07-25 14:56:31 +0200

    my first commit with GitHub action

---
+++ Program_test.py
@@ -0,0 +1,5 @@
+"""this is a test suite form my_program.py"""
+import my_program
+def  test_output():
+    """this adocstring"""
+    assert my_program.square_number(2)==4
---
+++ my_program.py
@@ -0,0 +1,4 @@
+"""this adocstring for the module"""
+def square_number(number):
+    """this a docstring for the function"""
+    return number**2
